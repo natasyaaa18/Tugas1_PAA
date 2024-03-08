3bd76746be022c218d752eaaa4d9a0c929662ce5
Natasya Ryka <118144287+natasyaaa18@users.noreply.github.com>
2024-03-08 18:41:04 +0700

    Add files via upload

---
+++ PersonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+
+        [HttpGet("api/person/{id}")]
+        public ActionResult<Person> GetPersonById(int id)
+        {
+            PersonContext context = new PersonContext();
+            Person person = context.GetPersonById(id);
+            return Ok(person);
+        }
+    }
+}
